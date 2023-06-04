c8456a2d62a00c0182f289e64dc31dc5212dad53
Jamie Kasulis <jlee.kasulis@gmail.com>
2023-06-04 16:45:18 -0400

    Add comment

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,6 +17,8 @@ app.get('/test', (req: any, res: any) => {
     res.send('TEST');
 });
 
+// The host must be 0.0.0.0 to work with Docker. 
+// See https://stackoverflow.com/questions/65721320/localhost-didn-t-send-any-data-err-empty-response-nodejs
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`)
 });
